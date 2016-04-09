b75a2e1b5428e5eb9f7293716bbfaad8c0e2d8f3
nyg0o0 <yaya56790@gmail.com>
2016-04-09 16:11:46 +0800

    new mouse click event

--- BeanGame.java
+++ BeanGame.java
@@ -14,11 +14,14 @@
 import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.*;
 import java.lang.Math;
+import javafx.animation.*;
+import javafx.util.Duration;
 
 // BeanGame class
 public class BeanGame extends Application {
@@ -26,11 +29,37 @@ public class BeanGame extends Application {
 	
 		// stage size
 		final double WIDTH = 400, HEIGHT = 400;
-		double centerX = WIDTH / 2, centerY = HEIGHT /2;	
+		double centerX = WIDTH / 2, centerY = HEIGHT /2;
+		int BALL_NUMS = 28;
+		
 		
 		// Create a new pane
 		Pane pane = new Pane();
-		
+		pane.setOnMouseClicked(e -> {
+			Group circles = new Group();
+			for(int i=0;i<7;i++){
+				Circle circle = new Circle(3,Color.RED);
+				circles.getChildren().add(circle);
+				//circle.setRadius(3);
+			//circle.setCenterX(centerX);
+			//circle.setCenterY(centerY - (Math.sqrt(3)/2)* 0.3* HEIGHT - 0.1* HEIGHT);
+			//circle.setFill(Color.RED);
+			//pane.getChildren().add(circle);
+			}
+			Timeline timeline = new Timeline();
+			Node circle = circles.getChildren().get(0);
+			timeline.getKeyFrames().addAll(
+			new KeyFrame(new Duration(5000),
+			new KeyValue(circle.translateXProperty(), 600),
+			new KeyValue(circle.translateYProperty(), 0)),
+			new KeyFrame(new Duration(10000),
+			new KeyValue(circle.translateXProperty(), 0),
+			new KeyValue(circle.translateYProperty(), 480)
+			));
+			
+			timeline.play();
+		});
+
 		/* Creat a new polyline */
 		Polyline polyline = new Polyline();
 		polyline.getPoints().addAll(new Double[]{	centerX- 0.05* WIDTH , centerY - (Math.sqrt(3)/2)* 0.3* HEIGHT - 0.1* HEIGHT,
@@ -55,9 +84,9 @@ public class BeanGame extends Application {
 		
 		/* Creat a new circle arrays */
 		Circle[] circle = new Circle[28];
-			for(int j=7;j>0;j--){
+			for( int j=7;j>0;j-- ){
 				for(int i=0;i<j;i++){
-					circle[i]=new Circle();
+					circle[i] = new Circle();
 					circle[i].setRadius(5.0);	// set the radius
 					circle[i].setLayoutX(centerX+ 0.0875* WIDTH*(i-3+0.5*(7-j)));	// set the circle's x coordinates
 					circle[i].setLayoutY(centerY + (Math.sqrt(3)/2)* 0.3* HEIGHT - 0.0875* WIDTH*(7-j)*0.85);	// set the circle's y coordinates
@@ -67,8 +96,12 @@ public class BeanGame extends Application {
 
 		/* Creat a scene and show it */
 		Scene scene = new Scene(pane,WIDTH,HEIGHT);	// create a scene for the pane
-		primaryStage.setTitle("Bean Machine Game interface");	// stage title
+		primaryStage.setTitle("Bean Machine Play");	// stage title
 		primaryStage.setScene(scene);	// put the pane on the stage
 		primaryStage.show();	// show it!
 	}
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
 }
+
